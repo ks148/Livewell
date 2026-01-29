a43fe08ef9d1e774edaddfeb2e0c9dcd3e749326
ks148 <ks@elitetackle.com>
2026-01-29 11:43:33 -0500

    Add files via upload
    Signed-off-by: ks148 <ks@elitetackle.com>

--- sw.js
+++ sw.js
@@ -1,9 +1,16 @@
-const CACHE_NAME = 'picklefinder-v1';
+const CACHE_NAME = 'livewell-v1';
 const ASSETS = [
-  './picklefinder.html',
+  './',
+  './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './bin/',
+  './bin/index.html',
+  './transfers/',
+  './transfers/index.html',
+  './putaway/',
+  './putaway/index.html'
 ];
 
 self.addEventListener('install', (event) => {
@@ -34,6 +41,6 @@ self.addEventListener('fetch', (event) => {
         if (req.method === 'GET') cache.put(req, copy);
       });
       return res;
-    }).catch(() => caches.match(req).then((m) => m || caches.match('./picklefinder.html')))
+    }).catch(() => caches.match(req).then((m) => m || caches.match('./')))
   );
 });
